a3634b2c47da2d76159343088d9e8d50ef871ffe
Michael Dreesen <michaeldreesen90@gmail.com>
2021-01-31 17:04:42 -0500

    added user post data

--- models/index.js
+++ models/index.js
@@ -1,3 +1,14 @@
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User }
+// create associations
+// Post does not belong to many users, this should only be with one user
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+// Post belongs to the user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+})
+
+module.exports = { User, Post };
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
+// force: true drops the db
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now Listening on PORT ${PORT}`));
 });
