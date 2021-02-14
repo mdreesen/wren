08ce648917b638b6d632552be26f67579571850f
Michael Dreesen <michaeldreesen90@gmail.com>
2021-02-13 19:32:27 -0500

    added Seen post data, need to add more.

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Seen = require('./Seen');
 
 // create associations
 // Post does not belong to many users, this should only be with one user
@@ -9,6 +10,20 @@ User.hasMany(Post, {
 // Post belongs to the user
 Post.belongsTo(User, {
     foreignKey: 'user_id',
-})
+});
+
+// -=- THIS WILL BE ASSOCIATED WITH THE ADMIN-=-
+// Seen belongs to many posts
+Seen.belongsTo(Post, {
+    through: Seen,
+    as: 'saw_post',
+    foreignKey: 'user_id'
+});
+
+Seen.belongsToMany(User, {
+    through: Seen,
+    as: 'saw_post',
+    foreignKey: 'post_id'
+});
 
 module.exports = { User, Post };
