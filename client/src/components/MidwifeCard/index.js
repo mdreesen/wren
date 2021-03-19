ee2f66451bf740340ba533826efc3e4461f0c0c8
Michael Dreesen <michaeldreesen90@gmail.com>
2021-03-18 23:15:12 -0400

    able to grab the midwife data from the db. Need to map through the array to stick the cards on the page.

--- client/src/components/MidwifeCard/index.js
+++ client/src/components/MidwifeCard/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function MidwifeCard() {
+
+const SearchMidwifeCard = (props) => {
+
+    // using useState to set the midwife card
+    // if there is nothing, return an empty array -> useState([]);
+    const [midwifeCard, setMidwifeCard] = useState([]);
+
+
+    // midwifeSearch function to get all midwife data from the database
+     const midwifeSearch = async () => {
+        const response = await fetch('/api/midwife');
+        const responseJson = await response.json();
+        setMidwifeCard(responseJson.results)
+        console.log(responseJson);
+    }
+
     return(
-        <div className="card" style={{width: '18rem'}}>
-            <img className="card-img-top" src="..." alt="Card image cap" />
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+        <div>
+        <>
+            <div className="card" style={{width: '18rem'}}>
+                <img className="card-img-top" src="..." alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">what</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
+        </>
+            <button onClick={midwifeSearch}>See all Midwives</button>
         </div>
+
     );
 }
 
-export default MidwifeCard;
+export default SearchMidwifeCard;
