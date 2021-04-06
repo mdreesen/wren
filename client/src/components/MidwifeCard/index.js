a453de21b8acb466b0ccd3ab477251c2cf844d54
Michael Dreesen <michaeldreesen90@gmail.com>
2021-04-05 22:23:44 -0400

    trying to loop through workers

--- client/src/components/MidwifeCard/index.js
+++ client/src/components/MidwifeCard/index.js
@@ -10,27 +10,40 @@ const SearchMidwifeCard = (props) => {
 
     // midwifeSearch function to get all midwife data from the database
      const midwifeSearch = async () => {
-        const response = await fetch('/api/midwife');
+        const response = await fetch('/wpi/worker');
         const responseJson = await response.json();
         setMidwifeCard(responseJson.results)
-        // console.log(responseJson);
+        console.log(responseJson);
+
+        const midwifeData = responseJson;
+        console.log(midwifeData);
+
+        return (
+            <div>
+            <>
+                {midwifeData.map(midwifeCard => (
+                <div key={midwifeCard.id} className="card" style={{width: '18rem'}}>
+                        <img className="card-img-top" src="..." alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title">{midwifeCard.firstname}</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                ))}
+                </>
+                <button onClick={midwifeSearch}>See all Midwives</button>
+            </div>
+            
+        );
     }
 
-    return(
-        <div>
-            <div className="card" style={{width: '18rem'}}>
-                <img className="card-img-top" src="..." alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">what</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+return(
+    <div>
+        <button onClick={midwifeSearch}>See all Midwives</button>
+    </div>
+);
 
-            <button onClick={midwifeSearch}>See all Midwives</button>
-        </div>
-
-    );
 }
 
 export default SearchMidwifeCard;
