3c57e521e2a7a81fcc2c05ae220ab02212d6e37f
Michael Dreesen <michaeldreesen90@gmail.com>
2021-04-10 22:21:40 -0400

    midwife card code cleanup

--- client/src/components/MidwifeCard/index.js
+++ client/src/components/MidwifeCard/index.js
@@ -4,20 +4,20 @@ import React from 'react';
 const SearchMidwifeCard = (props) => {
 
 
-        return (
-                    <>
-                    {props.midwifeCard.map((midwife, index) => (
-                        <div key={midwife.id} className="card" style={{width: '18rem'}}>
-                                <div className="card-body">
-                                    <h5 className="card-title">{midwife.firstname}</h5>
-                                    <h5 className="card-title">{midwife.lastname}</h5>
-                                    <p className="card-text">Description maybe?</p>
-                                    <a href="#" className="btn btn-primary">Affiliate</a>
-                                </div>
-                            </div>
-                    ))}
-                </>            
-        );
+    return (
+        <>
+            {props.midwifeCard.map((midwife, index) => (
+                <div key={midwife.id} className="card" style={{width: '18rem'}}>
+                    <div className="card-body">
+                        <h5 className="card-title">{midwife.firstname}</h5>
+                        <h5 className="card-title">{midwife.lastname}</h5>
+                        <p className="card-text">Description maybe?</p>
+                        <a href="#" className="btn btn-primary">Affiliate</a>
+                    </div>
+                </div>
+            ))}
+        </>            
+    );
 }
 
 export default SearchMidwifeCard;
