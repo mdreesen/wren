21d2df1d68c55a3f46463c22aca2e668216fc6f9
Michael Dreesen <michaeldreesen90@gmail.com>
2021-04-24 11:44:34 -0400

    added url

--- client/src/components/ErrorToast/index.js
+++ client/src/components/ErrorToast/index.js
@@ -3,18 +3,18 @@ import React from 'react';
 function ErrorToast() {
     return (
         <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
-  <div className="toast-header">
-    <img src="..." className="rounded mr-2" alt="..." />
-    <strong className="mr-auto">Bootstrap</strong>
-    <small className="text-muted">just now</small>
-    <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-      <span aria-hidden="true">×</span>
-    </button>
-  </div>
-  <div className="toast-body">
-    See? Just like this.
-  </div>
-</div>
+          <div className="toast-header">
+            <img src="..." className="rounded mr-2" alt="..." />
+            <strong className="mr-auto">Bootstrap</strong>
+            <small className="text-muted">just now</small>
+            <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div className="toast-body">
+            See? Just like this.
+          </div>
+        </div>
     );
 }
 
