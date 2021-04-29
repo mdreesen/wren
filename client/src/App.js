0828bc20dbc17847ebeea8aa6efd79986a286896
Michael Dreesen <michaeldreesen90@gmail.com>
2021-04-28 22:20:16 -0400

    setting up frontend to support graphql server

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, useParams, Redirect } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
 // Import pages
 import Landing from './pages/Landing';
@@ -11,23 +13,29 @@ import LoginMidwife from './components/LoginMidwife';
 import SecondaryLoginForm from './components/SecondaryLoginForm';
 import WorkerPage from './pages/WorkerPage';
 
+// making the connection to the graphql backend server
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql'
+});
+
 function App() {
 
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Landing}/>
-          <Route exact path="/settings/:user_id" component={UserInfoPage} />
-          <Route exact path="/home" component={Home}/>
-          <Route exact path="/worker-login" component={LoginMidwife}/>
-          <Route exact path="/user-login" component={SecondaryLoginForm} />
-          <Route exact path="/worker-home" component={WorkerPage}/>
-          <Route component={NotAPage}/>
-        </Switch>
-      </div>
-    </Router>
-
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Landing}/>
+            <Route exact path="/settings/:user_id" component={UserInfoPage} />
+            <Route exact path="/home" component={Home}/>
+            <Route exact path="/worker-login" component={LoginMidwife}/>
+            <Route exact path="/user-login" component={SecondaryLoginForm} />
+            <Route exact path="/worker-home" component={WorkerPage}/>
+            <Route component={NotAPage}/>
+          </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 export default App;
