5c6d1c6398073e8b3c03504488741a1e64f10623
Michael Dreesen <michaeldreesen90@gmail.com>
2021-05-10 21:35:11 -0400

    changed up the logged out navbar to have the back button instead of user navigation

--- client/src/components/NavbarUser/index.js
+++ client/src/components/NavbarUser/index.js
@@ -9,58 +9,14 @@ function Navbar() {
         Auth.logout();
     }
 
-    // const { user_id } = useParams();
-
-    // const [userInfo, setUserInfo] = useState({});
-
-
-    // const getUserId = async () => {
-    //     const user_url = `/api/users/${user_id}`
-    //     const response = await fetch(user_url);
-    //     const responseJson = await response.json();
-
-    //     setUserInfo(responseJson)
-    // }
-
-    // Logout function
-    // async function logout() {
-
-    //     document.querySelector('#logout-btn')
-
-    //     try {
-    //         const response = await fetch('/api/users/logout', {
-    //             method: 'post',
-    //             headers: { 'Content-type': 'application/json' }
-    //         });
-
-    //         if (response.ok) {
-    //             console.log(response);
-    //             localStorage.clear();
-    //             document.location.replace('/')
-    //         } else {
-    //             alert(response.status);
-    //         }
-    
-    //         console.log(response);
-
-    //     } catch (err) {
-    //         console.log(err.message);
-    //     }
-    // }
-
-    // const handleLogout = () => {
-    //     localStorage.clear();
-    //   };
-
-    // const { id: user_id } = useParams()
-    // console.log(user_id)
-
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse navbar-css" id="navbarNav">
+        {Auth.loggedIn() ? (
+            <>
             <ul className="navbar-nav">
                 <li className="nav-item">
                     <Link to="/home" className="nav-link">
@@ -81,6 +37,17 @@ function Navbar() {
                 <Link to="/" className="nav-link" onClick={logout}>Logout</Link>
                 </li>
             </ul>
+            </>
+        ) : (
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to="/" className="nav-link">
+                        back
+                    </Link>
+                </li>
+            </ul>
+        )}
+
         </div>
         </nav>
 
