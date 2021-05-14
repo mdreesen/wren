7e26fc220a73543e65aa8e7a95385f35e8cc270a
Michael Dreesen <michaeldreesen90@gmail.com>
2021-05-13 23:41:47 -0400

    one last push

--- client/src/components/LoginMidwife/index.js
+++ client/src/components/LoginMidwife/index.js
@@ -1,8 +1,8 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import Auth from '../../utils/auth';
-// import { useMutation } from '@apollo/react-hooks';
-// import { LOGIN_BIRTHWORKER } from '../../utils/mutations';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
+import { useMutation } from '@apollo/react-hooks';
+import { LOGIN_BIRTHWORKER } from '../../utils/mutations';
 
 
 // TODO
@@ -10,66 +10,66 @@
 // UnhandledPromiseRejectionWarning: Error: WHERE parameter "email" has invalid "undefined" value
 // it is switching to other pages after email and password is put in, but need to handle this better
 
-// function LoginMidwife() {
+function LoginMidwife() {
 
-//     const [formState, setFormState] = useState({ email: '', password: '' });
-//     const [workerLogin, { error }] = useMutation(LOGIN_BIRTHWORKER);
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [workerLogin, { error }] = useMutation(LOGIN_BIRTHWORKER);
   
-//     // update state based on form input changes
-//     const handleChange = event => {
-//       const { name, value } = event.target;
+    // update state based on form input changes
+    const handleChange = event => {
+      const { name, value } = event.target;
   
-//       setFormState({
-//         ...formState,
-//         [name]: value
-//       });
-//     };
+      setFormState({
+        ...formState,
+        [name]: value
+      });
+    };
   
-//     // submit form
-//     const loginFormHandler = async event => {
-//       event.preventDefault();
+    // submit form
+    const loginFormHandler = async event => {
+      event.preventDefault();
   
-//       try {
-//         const { data } = await workerLogin({
-//           variables: { ...formState }
-//         });
-//         console.log(data)
-//         Auth.login(data.workerLogin.token);
-//       } catch (e) {
-//         console.error(e);
-//       }
+      try {
+        const { data } = await workerLogin({
+          variables: { ...formState }
+        });
+        console.log(data)
+        Auth.login(data.workerLogin.token);
+      } catch (e) {
+        console.error(e);
+      }
   
-//       // clear form values
-//       setFormState({
-//         email: '',
-//         password: ''
-//       });
-//     };
+      // clear form values
+      setFormState({
+        email: '',
+        password: ''
+      });
+    };
 
-//     return(
-//         <div className="workerPage">
-//             <div className="workerFormContainer">
-//             <div className="formContainer">
-//                 <form className="midwifeForm">
-//                     <h3>Worker Login</h3>
-//                     <div className="form-group">
-//                         <label for="workerEmail">Email address</label>
-//                         <input type="email" name="email" value={formState.email} onChange={handleChange} className="form-control" id="worker-login-email" aria-describedby="emailInput" placeholder="Enter email" />
-//                     </div>
-//                     <div className="form-group">
-//                         <label for="workerPassword">Password</label>
-//                         <input type="password" name="password" value={formState.password} onChange={handleChange} className="form-control" id="worker-login-password" placeholder="Password" />
-//                     </div>
-//                     <div className="button-container">
-//                         <Link to="/" className="btn btn-primary">back</Link>
-//                         <button type="submit" className="btn btn-primary" onClick={loginFormHandler}>Login</button>
-//                     </div>
-//                     {error && <div>Incorrect Login</div>}
-//                 </form>
-//             </div>
-//         </div>
-//    </div>
-//     );
-// }
+    return(
+        <div className="workerPage">
+            <div className="workerFormContainer">
+            <div className="formContainer">
+                <form className="midwifeForm">
+                    <h3>Worker Login</h3>
+                    <div className="form-group">
+                        <label for="workerEmail">Email address</label>
+                        <input type="email" name="email" value={formState.email} onChange={handleChange} className="form-control" id="worker-login-email" aria-describedby="emailInput" placeholder="Enter email" />
+                    </div>
+                    <div className="form-group">
+                        <label for="workerPassword">Password</label>
+                        <input type="password" name="password" value={formState.password} onChange={handleChange} className="form-control" id="worker-login-password" placeholder="Password" />
+                    </div>
+                    <div className="button-container">
+                        <Link to="/" className="btn btn-primary">back</Link>
+                        <button type="submit" className="btn btn-primary" onClick={loginFormHandler}>Login</button>
+                    </div>
+                    {error && <div>Incorrect Login</div>}
+                </form>
+            </div>
+        </div>
+   </div>
+    );
+}
 
-// export default LoginMidwife;
+export default LoginMidwife;
