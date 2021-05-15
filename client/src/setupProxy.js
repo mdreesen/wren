133c1926714dab13f0cbedb38a1e6e6107a67d5a
Michael Dreesen <michaeldreesen90@gmail.com>
2021-05-15 01:08:46 -0400

    d

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
-// const proxy = require("http-proxy-middleware");
+const proxy = require("http-proxy-middleware");
 
-// module.exports = function(app) {
-//   app.use(
-//     proxy(["/"], { target: process.env.ROUTE})
-//   );
-// };
+module.exports = function(app) {
+  app.use(
+    proxy(["/"], { target: process.env.ROUTE})
+  );
+};
