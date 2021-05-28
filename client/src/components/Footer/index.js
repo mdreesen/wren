89d3d6939c8a6e4cf15074e7c8c013416834a549
Michael Dreesen <michaeldreesen90@gmail.com>
2021-05-27 23:17:23 -0400

    changing the footer navs and styling

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,17 +8,18 @@ function Footer() {
     {/* <a className="navbar-brand" href="#">Hidden brand</a> */}
     <div className="icon-left">
         <ul className="icon-column">
-            <li>icon left</li>
-            <li>icon left</li>
-            <li>icon left</li>
-            <li><Link to="/worker-login">worker login</Link></li>
+            <a className="footer-link"><Link to="">About</Link></a>
+            <a><Link to="/worker-login">worker login</Link></a>
+            <a className="footer-link"><Link to="/"/></a>
+
+            <li className="footer-link">copyright of Wren&copy;</li>
         </ul>
     </div>
     <div className="icon-right">
         <ul className="icon-column">
-            <a>icon1</a>
-            <a>icon2</a>
-            <a>icon3</a>
+            <a className="footer-link"><Link to="/" class="fab fa-instagram fa-lg"></Link></a>
+            <a className="footer-link"><Link to="/" className="fab fa-facebook fa-lg" /></a>
+            <a className="footer-link"><Link to="/" className="fab fa-twitter fa-lg" /></a>
         </ul>
     </div>
 </nav>
