f8a35133c386cc57239aaf93a72e242f494b5389
Michael Dreesen <michaeldreesen90@gmail.com>
2022-01-18 21:35:58 -0500

    added questions page and component (getting started)

---
+++ client/src/components/Questions/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import questionData from '../../utils/question.json';
+
+const Questions = () => {
+
+    console.log(questionData);
+
+    return (
+        <div>Questions</div>
+    );
+};
+
+export default Questions;
---
+++ client/src/pages/PatientQuestions/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ClientQuestions = () => {
+    return (
+        <div>Client Questions</div>
+    );
+};
+
+export default ClientQuestions;
